fb9da34e7621dc539702e0887f4ec4b6cb14bfb3
edimilson braz <edimilson.gt8@gmail.com>
2023-05-26 19:38:39 -0300

    :sparkles: GET - Protocolo HTTP Integrando do Back ao Front com Controllers

--- versao-base/pages/index.tsx
+++ versao-base/pages/index.tsx
@@ -1,9 +1,24 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useEffect, useState } from "react";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
+import { todoController } from "@ui/controller/todo";
 
 const bg = "/bg.jpeg";
 
+interface HomeTopo {
+  id: string;
+  content: string;
+}
+
 function HomePage() {
+  const [todos, setTodos] = useState<HomeTopo[]>([]);
+
+  useEffect(() => {
+    todoController.get().then((todos) => {
+      setTodos(todos);
+    });
+  }, []);
+
   return (
     <main>
       <GlobalStyles themeName="indigo" />
@@ -41,24 +56,22 @@ function HomePage() {
           </thead>
 
           <tbody>
-            <tr>
-              <td>
-                <input type="checkbox" />
-              </td>
-              <td>d4f26</td>
-              <td>
-                Conteúdo de uma TODO Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque vero facilis obcaecati, autem aliquid
-                eius! Consequatur eaque doloribus laudantium soluta optio odit,
-                provident, ab voluptates doloremque voluptas recusandae
-                aspernatur aperiam.
-              </td>
-              <td align="right">
-                <button data-type="delete">Apagar</button>
-              </td>
-            </tr>
+            {todos.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td>
+                    <input type="checkbox" />
+                  </td>
+                  <td>{todo.id.substring(0, 5)}</td>
+                  <td>{todo.content}</td>
+                  <td align="right">
+                    <button data-type="delete">Apagar</button>
+                  </td>
+                </tr>
+              );
+            })}
 
-            <tr>
+            {/* <tr>
               <td colSpan={4} align="center" style={{ textAlign: "center" }}>
                 Carregando...
               </td>
@@ -68,9 +81,9 @@ function HomePage() {
               <td colSpan={4} align="center">
                 Nenhum item encontrado
               </td>
-            </tr>
+            </tr> */}
 
-            <tr>
+            {/* <tr>
               <td colSpan={4} align="center" style={{ textAlign: "center" }}>
                 <button data-type="load-more">
                   Carregar mais{" "}
@@ -85,7 +98,7 @@ function HomePage() {
                   </span>
                 </button>
               </td>
-            </tr>
+            </tr> */}
           </tbody>
         </table>
       </section>
